1ee57d1dc2ef38c2456ec18c2c5ac8fc62478332
Robert Stein <robert.stein@desy.de>
2020-12-02 14:08:16 +0100

    Remove old files.

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,3 +1,8 @@
-fontsize = 12.5
-big_fontsize = fontsize * 1.5
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "sans-serif"
+
+fontsize = 16.0
+big_fontsize = 19.0
 fig_width = 6
+golden_ratio = 1.618
+fig_height = fig_width/golden_ratio
