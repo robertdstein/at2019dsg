3cd5bd81e92f47df28ac01202b33854121a6359c
Robert Stein <robert.stein@desy.de>
2020-12-05 19:58:17 +0100

    Add tqdm

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setuptools.setup(
         "pylatex",
         "ffmpeg-python",
         "gammapy",
+        "tqdm",
         "sjoert @ https://github.com/sjoertvv/sjoert/archive/3308c1afe111693da0821b6d8b24a0439f0a648c.zip",
         "k3match @ https://github.com/pschella/k3match/archive/master.zip"
     ],
